b3dc04bfd26dda18727a7b454400d6b8d54fab75
ullrichm04 <72552376+markusuW-04@users.noreply.github.com>
2021-11-16 03:46:53 +0100

    CommitOf202111160346
    Final commit before Review

--- New Unity Project (1)/Assets/timerscript.cs
+++ New Unity Project (1)/Assets/timerscript.cs
@@ -5,26 +5,40 @@ using UnityEngine;
 
 public class timerscript : MonoBehaviour
 {
-    public int othercount = 0;
-    public int timecount = 0000;
-    int ticknum = 10000000;
+    double timer = 0.0f;
+    double seconds;
+    int last;
     // Start is called before the first frame update
     void Start()
     {
+        Debug.Log("lolstart");
     }
     // Update is called once per frame
     void Update()
     {
-        othercount++;
-        if (othercount % ticknum == 0)
-        {
-            timecount++;
-            Console.WriteLine(timecount);
-        }
+        MyTimer();
+        
     }
 
     void MyTimer()
     {
-        
+        timer += Time.deltaTime;
+        seconds = Math.Floor(timer % 60);
+        if (last != Convert.ToInt32(seconds))
+        {
+            last = Convert.ToInt32(seconds);
+            Debug.Log(seconds);
+        }
+    }
+
+    
+}
+
+public static class myfun
+{
+    public static bool IsBetween<T>(this T item, T start, T end)
+    {
+        return Comparer<T>.Default.Compare(item, start) >= 0
+            && Comparer<T>.Default.Compare(item, end) <= 0;
     }
 }
